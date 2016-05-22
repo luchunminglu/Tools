26370db3268fdad60cfa709277fa3ebc22f8d138
luchunminglu <luchunminglu@gmail.com>
2016-05-22 13:16:27 +0800

    x

---
+++ Tools/GuardTool/GuardServiceInstaller.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuardTool
+{
+
+    [RunInstaller(true)]
+    public class GuardServiceInstaller:Installer
+    {
+        public const string ServiceName = "GeneralGuardService";
+
+        public GuardServiceInstaller()
+        {
+            ServiceProcessInstaller serviceProcessInstaller = new ServiceProcessInstaller();
+            ServiceInstaller serviceInstaller = new ServiceInstaller();
+
+            //设置service count信息
+            serviceProcessInstaller.Account = ServiceAccount.LocalSystem;
+            serviceProcessInstaller.Username = null;
+            serviceProcessInstaller.Password = null;
+
+            //#service information
+        }
+
+    }
+}
